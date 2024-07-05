ba90b58246ef1800f20d91fb6137c28aa54b0f4f
KarlBrion <karl.brion13@gmail.com>
2024-07-05 20:19:01 +0800

    Update match.py

--- phasebook/match.py
+++ phasebook/match.py
@@ -3,25 +3,24 @@ from flask import Blueprint
 
 from .data.match_data import MATCHES
 
-
 bp = Blueprint("match", __name__, url_prefix="/match")
-
 
 @bp.route("<int:match_id>")
 def match(match_id):
     if match_id < 0 or match_id >= len(MATCHES):
         return "Invalid match id", 404
 
+    fave_numbers_1, fave_numbers_2 = MATCHES[match_id]
+
     start = time.time()
-    msg = "Match found" if (is_match(*MATCHES[match_id])) else "No match"
+    is_matched = is_match(fave_numbers_1, fave_numbers_2)
     end = time.time()
 
+    msg = "Match found" if is_matched else "No match"
+
     return {"message": msg, "elapsedTime": end - start}, 200
-
-
 def is_match(fave_numbers_1, fave_numbers_2):
-    for number in fave_numbers_2:
-        if number not in fave_numbers_1:
-            return False
+    set_fave_numbers_1 = set(fave_numbers_1)
+    set_fave_numbers_2 = set(fave_numbers_2)
 
-    return True
+    return set_fave_numbers_2 <= set_fave_numbers_1
